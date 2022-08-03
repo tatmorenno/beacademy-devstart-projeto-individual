23c1eb19ba76e404f96725e32c0d2952699e3041
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-08-03 17:00:07 -0300

    Controllers, seeders e factories

--- app-individual-laravel/app/Http/Controllers/UserController.php
+++ app-individual-laravel/app/Http/Controllers/UserController.php
@@ -2,24 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = [
-            'nomes' => [
-                'Tatiana Moreno',
-                'João Neto',
-            ]
-        ];
-
-        dd($users);
+        $users = User::all();
+        return view('users.index', compact('users'));
     }
 
     public function show($id)
     {
-        dd('Id do usuário é: ' . $id);
+        if(!$user = User::find($id))
+            return redirect()->route('users.index');
+            $title = 'Usuário ' . $user->name;
+
+        return view('users.show', compact('user', 'title')); 
+    }
+
+    public function create()
+    {
+        dd('here');
     }
 }
