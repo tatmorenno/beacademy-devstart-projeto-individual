88fd1f4b7d9c674d1e538ed764e1b401bde4daba
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-08-04 01:56:20 -0300

    Controllers

--- app-individual-laravel/app/Http/Controllers/UserController.php
+++ app-individual-laravel/app/Http/Controllers/UserController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreUpdateUserFormRequest;
 
 class UserController extends Controller
 {
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+    }
+
     public function index()
     {
         $users = User::all();
@@ -23,6 +30,56 @@ class UserController extends Controller
 
     public function create()
     {
-        dd('here');
+        return view('users.create');
+    }
+
+    public function store(StoreUpdateUserFormRequest $request)
+    {
+        // $user = new User;
+        // $user->name = $request->name;
+        // $user->email = $request->email;
+        // $user->password = bcrypt($request->password);
+        // $user->save();
+
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+
+        $this->model->create($data);
+
+        return redirect()->route('users.index');
+    }
+
+    public function edit($id)
+    {
+        if(!$user = $this->model->find($id))
+            return redirect()->route('users.index');
+
+        return view('users.edit', compact('user'));
+
+    }
+
+    public function update(StoreUpdateUserFormRequest $request, $id)
+    {
+        if(!$user = $this->model->find($id))
+        return redirect()->route('users.index');
+        $data = $request->only('name', 'email');
+        if($request->password)
+            $data['password']= bcrypt($request->password);
+        
+        $user->update($data);
+
+        return redirect()->route('users.index');
+    }
+
+    public function destroy($id)
+    {
+        if(!$user = $this->model->find($id))
+        return redirect()->route('users.index');
+
+        $user->delete();
+
+        return redirect()->route('users.index');
+
+
     }
 }
